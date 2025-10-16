434f3283486c069ec55c318353d5a66bff8f0136
Man <nguyenphanminhman04@gmail.com>
2025-10-16 17:20:52 +0700

    Bai02

---
+++ NguyenPhanMinhMan_22679171_Week08/NguyenPhanMinhMan_22679171_Bai02/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, TouchableOpacity,  Image} from "react-native";
+
+
+export default function Start() {
+    return (
+        
+<View>
+
+<View>
+    <Image source={require('../assets/nhatky.png')}/>
+</View>
+</View>
+    );
+}
